fc9f0f990c55402fbb990f72fcfa0ff32accb403
Rishikesh D Mhatre <rishikesh.mhatre@bitwiseglobal.com>
2022-10-04 09:21:59 +0530

    first commit

--- src/ComputeEmployeeWage.java
+++ src/ComputeEmployeeWage.java
@@ -1,10 +1,5 @@
 public class ComputeEmployeeWage {
     public static void main(String[] args) {
-        double random = Math.random();
-        if (random > .5) {
-            System.out.println("Employee is present");
-        } else {
-            System.out.println("Employee is absent");
-        }
+        System.out.println("Welcome to employee wage");
     }
 }
