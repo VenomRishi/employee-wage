2087af85f19eec208ff1a72ddb0b58a1db01ab09
Rishikesh D Mhatre <rishikesh.mhatre@bitwiseglobal.com>
2022-10-04 09:23:52 +0530

    added: logic to check employee is present or absent

--- src/ComputeEmployeeWage.java
+++ src/ComputeEmployeeWage.java
@@ -1,5 +1,11 @@
 public class ComputeEmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage");
+        double random = Math.random();
+        if (random > .5) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is absent");
+        }
     }
 }
