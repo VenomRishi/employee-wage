556eb0f78dc35561fde21659aec575e6548d41c9
Rishikesh D Mhatre <rishikesh.mhatre@bitwiseglobal.com>
2022-10-04 09:36:35 +0530

    added: logic for calculating daily wage

--- src/ComputeEmployeeWage.java
+++ src/ComputeEmployeeWage.java
@@ -1,9 +1,12 @@
 public class ComputeEmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage");
+        int wagePerHour = 20;
+        int fullDayHour = 8;
         double random = Math.random();
         if (random > .5) {
             System.out.println("Employee is present");
+            System.out.println("Daily wage is : " + wagePerHour * fullDayHour);
         } else {
             System.out.println("Employee is absent");
         }
